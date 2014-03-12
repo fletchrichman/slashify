162cb2b730573f76e8034306f3b4ad9cbcd49fda
Fletcher Richman <frichman3@gmail.com>
2014-03-12 11:05:40 -0500

    added twitter omniauth and authentication model

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  provider :facebook, ENV['SLASHIFY_FACEBOOK_KEY'], ENV['SLASHIFY_FACEBOOK_SECRET'], :scope => 'email, publish_actions'
+  provider :twitter, ENV['SLASHIFY_TWITTER_KEY'], ENV['SLASHIFY_TWITTER_SECRET']
 end
