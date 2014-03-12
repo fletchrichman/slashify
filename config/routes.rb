2d7c2e0569f2589549d441d9146eddd4d16c2bb1
Fletcher Richman <frichman3@gmail.com>
2014-03-12 14:59:10 -0500

    added pages controller and basic homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Slashify::Application.routes.draw do
   get 'auth/:provider/callback', to: 'authenticate#create'
   get "authenticate/index"
   get "authenticate/create"
+
+  root to: "pages#home"
 end
